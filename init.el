46265987a3195c9d65ecb773caa22e9bfb15385c
Kieran Healy <kjhealy@gmail.com>
2014-07-18 11:17:37 -0400

    don't add ~/.emacs.d/ to the load-path (recent 24.4 builds complain about this)

--- init.el
+++ init.el
@@ -7,7 +7,6 @@
 
 ;; setq dotfiles-dir (file-name-directory (or (buffer-file-name) load-file-name)))
 (setq dotfiles-dir (file-name-directory (or load-file-name (buffer-file-name))))
-(add-to-list 'load-path dotfiles-dir)
 (setq autoload-file (concat dotfiles-dir "loaddefs.el"))
 (setq package-user-dir (concat dotfiles-dir "elpa"))
 (setq custom-file (concat dotfiles-dir "custom.el"))
