bcea22b41dee07f57c2cc972d0a7d9df7aa02f01
dgm <dgm@officePC.uned.es>
2018-10-29 11:28:51 +0100

    .elc files includes I am trying to include the ELPA folder but it doesn't show!

---
+++ custom.el
@@ -0,0 +1,38 @@
+(custom-set-variables
+ ;; custom-set-variables was added by Custom.
+ ;; If you edit it by hand, you could mess it up, so be careful.
+ ;; Your init file should contain only one such instance.
+ ;; If there is more than one, they won't work right.
+ '(LaTeX-XeTeX-command "xelatex -synctex=1")
+ '(TeX-engine 'xetex)
+ '(blink-cursor-mode nil)
+ '(helm-completing-read-handlers-alist
+ '((describe-function . helm-completing-read-symbols)
+   (describe-variable . helm-completing-read-symbols)
+   (describe-symbol . helm-completing-read-symbols)
+   (debug-on-entry . helm-completing-read-symbols)
+   (find-function . helm-completing-read-symbols)
+   (disassemble . helm-completing-read-symbols)
+   (trace-function . helm-completing-read-symbols)
+   (trace-function-foreground . helm-completing-read-symbols)
+   (trace-function-background . helm-completing-read-symbols)
+   (find-tag . helm-completing-read-default-find-tag)
+   (org-capture . helm-org-completing-read-tags)
+   (org-set-tags . helm-org-completing-read-tags)
+   (ffap-alternate-file)
+   (tmm-menubar)
+   (find-file)
+   (find-file-at-point . helm-completing-read-sync-default-handler)
+   (ffap . helm-completing-read-sync-default-handler)
+   (execute-extended-command)))
+ '(org-agenda-files
+ '("/home/dgm/Dropbox/gtd/inbox.org" "/home/dgm/Dropbox/gtd/gtd.org" "/home/dgm/Dropbox/gtd/journal.org" "/media/dgm/blue/documents/proyectos/mtj/mtj_gtd.org" "/media/dgm/blue/documents/proyectos/iat_methods/iat_methods.org" "/media/dgm/blue/documents/proyectos/laBussola/laBussola_gtd.org" "/home/dgm/Dropbox/gtd/tickler.org"))
+ '(package-selected-packages
+ '(gpastel dashboard org-ref redo+ ztree zenburn-theme yasnippet-snippets yasnippet-classic-snippets yaml-mode which-key volatile-highlights visual-regexp use-package unicode-fonts undo-tree typit textmate tango-2-theme speed-type spacemacs-theme spaceline soothe-theme solarized-theme smex smartparens recentf-ext rebox2 rainbow-mode r-autoyas python-x python-mode py-autopep8 poly-noweb poly-markdown poly-R pandoc-mode ox-twbs ox-tufte ox-pandoc ox-gfm org-plus-contrib org-ac ob-ipython nyan-mode multiple-cursors monokai-theme moe-theme maxframe material-theme magit live-py-mode lenlen-theme latex-pretty-symbols labburn-theme kurecolor jedi inf-ruby iedit idle-highlight-mode ibuffer-vc htmlize highlight-symbol highlight-numbers helpful helm-projectile helm-gtags helm-exwm helm-descbinds helm-bibtex helm-ag helm-R hc-zenburn-theme gruvbox-theme golden-ratio geiser free-keys flycheck-tip flx-ido eyebrowse expand-region exec-path-from-shell eval-in-repl emmet-mode elpy ein ebib duplicate-thing discover-my-major diff-hl deft darktooth-theme darkokai-theme csv-mode company-web company-statistics company-quickhelp company-math company-auctex color-theme-sanityinc-tomorrow color-theme-sanityinc-solarized color-theme-modern clues-theme clean-aindent-mode browse-kill-ring anti-zenburn-theme ag adaptive-wrap helm helm-descbinds helm-ls-git flycheck helm-flycheck helm-system-packages exwm helm-exwm pulseaudio-control org-plus-contrib org-bullets helm-org-contacts helm helm-descbinds helm-ls-git flycheck helm-flycheck helm-system-packages exwm helm-exwm pulseaudio-control org-plus-contrib org-bullets helm-org-contacts))
+ '(text-mode-hook '(text-mode-hook-identify)))
+(custom-set-faces
+ ;; custom-set-faces was added by Custom.
+ ;; If you edit it by hand, you could mess it up, so be careful.
+ ;; Your init file should contain only one such instance.
+ ;; If there is more than one, they won't work right.
+ )
