a70b541a0914c8347f05ea037ac3b34e34b83250
dgm@laptop <daniel.guinea.uned@gmail.com>
2019-10-21 22:09:33 +0200

    Further customization of mu4e

--- custom.el
+++ custom.el
@@ -3,15 +3,78 @@
  ;; If you edit it by hand, you could mess it up, so be careful.
  ;; Your init file should contain only one such instance.
  ;; If there is more than one, they won't work right.
+ '(ansi-color-names-vector
+ ["#eee8d5" "#dc322f" "#859900" "#b58900" "#268bd2" "#d33682" "#2aa198" "#839496"])
  '(atomic-chrome-url-major-mode-alist
  '(("reddit\\.com" . markdown-mode)
    ("uned\\.es" . text-mode)))
  '(bmkp-last-as-first-bookmark-file "~/.emacs.d/bookmarks")
+ '(compilation-message-face 'default)
+ '(cua-global-mark-cursor-color "#2aa198")
+ '(cua-normal-cursor-color "#657b83")
+ '(cua-overwrite-cursor-color "#b58900")
+ '(cua-read-only-cursor-color "#859900")
+ '(fci-rule-color "#eee8d5")
  '(helm-source-names-using-follow nil)
+ '(highlight-changes-colors '("#d33682" "#6c71c4"))
+ '(highlight-symbol-colors
+ (--map
+  (solarized-color-blend it "#fdf6e3" 0.25)
+  '("#b58900" "#2aa198" "#dc322f" "#6c71c4" "#859900" "#cb4b16" "#268bd2")))
+ '(highlight-symbol-foreground-color "#586e75")
+ '(highlight-tail-colors
+ '(("#eee8d5" . 0)
+   ("#B4C342" . 20)
+   ("#69CABF" . 30)
+   ("#69B7F0" . 50)
+   ("#DEB542" . 60)
+   ("#F2804F" . 70)
+   ("#F771AC" . 85)
+   ("#eee8d5" . 100)))
+ '(hl-bg-colors
+ '("#DEB542" "#F2804F" "#FF6E64" "#F771AC" "#9EA0E5" "#69B7F0" "#69CABF" "#B4C342"))
+ '(hl-fg-colors
+ '("#fdf6e3" "#fdf6e3" "#fdf6e3" "#fdf6e3" "#fdf6e3" "#fdf6e3" "#fdf6e3" "#fdf6e3"))
+ '(hl-paren-colors '("#2aa198" "#b58900" "#268bd2" "#6c71c4" "#859900"))
+ '(nrepl-message-colors
+ '("#dc322f" "#cb4b16" "#b58900" "#546E00" "#B4C342" "#00629D" "#2aa198" "#d33682" "#6c71c4"))
  '(org-contacts-files '("/home/dgm/Dropbox/gtd/contacts.org"))
  '(package-selected-packages
  '(buffer-flip pomidor palimpsest origami org-sidebar org-ql quelpa-use-package quelpa helm-org focus org-super-agenda gnu-elpa-keyring-update ghub+ pass ivy-pass zygospore ztree yasnippet-snippets yasnippet-classic-snippets wttrin ws-butler worf wordnut which-key volatile-highlights visual-regexp visual-fill-column vi-tilde-fringe use-package undo-tree undo-propose typing twittering-mode try synosaurus switch-window sudo-edit stripe-buffer speed-type sos solarized-theme smooth-scrolling smartscan smartparens shx shell-pop scratch recentf-ext rebox2 rainbow-mode rainbow-delimiters racket-mode python-mode py-autopep8 pulseaudio-control powerline poly-R pandoc-mode page-break-lines ox-twbs ox-pandoc org-wiki org-web-tools org-ref org-plus-contrib org-mime org-bullets olivetti nyan-mode multiple-cursors multi mu4e-conversation moody minions miniedit mark-multiple magit live-py-mode ledger-mode latex-pretty-symbols kurecolor ivy-yasnippet interleave iedit ido-vertical-mode highlight-symbol highlight-numbers helpful helm-swoop helm-projectile helm-pass helm-org-rifle helm-mu helm-gtags helm-exwm helm-descbinds helm-ag helm-ack guide-key gpastel ghub ggtags forecast flycheck exwm-edit expand-region exec-path-from-shell eval-in-repl ess-view ess-smart-underscore ess-R-data-view engine-mode elpy editorconfig edit-server edit-list duplicate-thing dumb-jump dtrt-indent dired-narrow diminish diff-hl deft company-shell company-jedi company-c-headers company-auctex color-theme-solarized clean-aindent-mode cheatsheet browse-kill-ring beacon atomic-chrome anzu ag))
- '(pdf-misc-print-programm "/usr/bin/gtklp"))
+ '(pdf-misc-print-programm "/usr/bin/gtklp")
+ '(pos-tip-background-color "#eee8d5")
+ '(pos-tip-foreground-color "#586e75")
+ '(smartrep-mode-line-active-bg (solarized-color-blend "#859900" "#eee8d5" 0.2))
+ '(term-default-bg-color "#fdf6e3")
+ '(term-default-fg-color "#657b83")
+ '(vc-annotate-background nil)
+ '(vc-annotate-background-mode nil)
+ '(vc-annotate-color-map
+ '((20 . "#dc322f")
+   (40 . "#c8805d801780")
+   (60 . "#bec073400bc0")
+   (80 . "#b58900")
+   (100 . "#a5008e550000")
+   (120 . "#9d0091000000")
+   (140 . "#950093aa0000")
+   (160 . "#8d0096550000")
+   (180 . "#859900")
+   (200 . "#66aa9baa32aa")
+   (220 . "#57809d004c00")
+   (240 . "#48559e556555")
+   (260 . "#392a9faa7eaa")
+   (280 . "#2aa198")
+   (300 . "#28669833af33")
+   (320 . "#279993ccbacc")
+   (340 . "#26cc8f66c666")
+   (360 . "#268bd2")))
+ '(vc-annotate-very-old-color nil)
+ '(weechat-color-list
+ '(unspecified "#fdf6e3" "#eee8d5" "#990A1B" "#dc322f" "#546E00" "#859900" "#7B6000" "#b58900" "#00629D" "#268bd2" "#93115C" "#d33682" "#00736F" "#2aa198" "#657b83" "#839496"))
+ '(xterm-color-names
+ ["#eee8d5" "#dc322f" "#859900" "#b58900" "#268bd2" "#d33682" "#2aa198" "#073642"])
+ '(xterm-color-names-bright
+ ["#fdf6e3" "#cb4b16" "#93a1a1" "#839496" "#657b83" "#6c71c4" "#586e75" "#002b36"]))
 (custom-set-faces
  ;; custom-set-faces was added by Custom.
  ;; If you edit it by hand, you could mess it up, so be careful.
