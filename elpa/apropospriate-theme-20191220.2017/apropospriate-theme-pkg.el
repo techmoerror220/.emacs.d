b0352920ffabb49ddb01772ef11f50e1c8aafbe1
dgm@laptop <daniel.guinea.uned@gmail.com>
2020-04-17 23:49:50 +0200

    Tweaking themes

---
+++ elpa/apropospriate-theme-20191220.2017/apropospriate-theme-pkg.el
@@ -0,0 +1,6 @@
+(define-package "apropospriate-theme" "20191220.2017" "A colorful, low-contrast, light & dark theme set for Emacs with a fun name." 'nil :keywords
+  '("color" "theme")
+  :url "https://github.com/waymondo/apropospriate-theme")
+;; Local Variables:
+;; no-byte-compile: t
+;; End:
