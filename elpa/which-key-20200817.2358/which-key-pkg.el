846b3cd687d1ad8ae7eee2da084d675eef5c6c89
dgm@laptop <daniel.guinea.uned@gmail.com>
2020-09-18 22:00:01 +0200

    Emacs packages updated

--- elpa/which-key-20200817.2358/which-key-pkg.el
+++
@@ -1,2 +0,0 @@
-;;; -*- no-byte-compile: t -*-
-(define-package "which-key" "20200817.2358" "Display available keybindings in popup" '((emacs "24.4")) :commit "e48e190a75a0c176e1deac218b891e77792d6921" :authors '(("Justin Burkett" . "justin@burkett.cc")) :maintainer '("Justin Burkett" . "justin@burkett.cc") :url "https://github.com/justbur/emacs-which-key")
