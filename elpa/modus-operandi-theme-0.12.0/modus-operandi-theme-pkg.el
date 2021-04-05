9e1f9347c4ddbca9f9597b4cc2e3698143dc28d2
dgm@laptop <daniel.guinea.uned@gmail.com>
2021-04-05 16:34:54 +0200

    Got rid of elpa/ and quelpa/ in the git repo

--- elpa/modus-operandi-theme-0.12.0/modus-operandi-theme-pkg.el
+++
@@ -1,2 +0,0 @@
-;;; -*- no-byte-compile: t -*-
-(define-package "modus-operandi-theme" "0.12.0" "Accessible light theme (WCAG AAA)" '((emacs "26.1")) :keywords '("faces" "theme" "accessibility") :authors '(("Protesilaos Stavrou" . "info@protesilaos.com")) :maintainer '("Protesilaos Stavrou" . "info@protesilaos.com") :url "https://gitlab.com/protesilaos/modus-themes")
--- elpa/modus-vivendi-theme-0.12.0/modus-vivendi-theme-pkg.el
+++
@@ -1,2 +0,0 @@
-;;; -*- no-byte-compile: t -*-
-(define-package "modus-vivendi-theme" "0.12.0" "Accessible dark theme (WCAG AAA)" '((emacs "26.1")) :keywords '("faces" "theme" "accessibility") :authors '(("Protesilaos Stavrou" . "info@protesilaos.com")) :maintainer '("Protesilaos Stavrou" . "info@protesilaos.com") :url "https://gitlab.com/protesilaos/modus-themes")
